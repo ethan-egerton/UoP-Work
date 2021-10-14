88f6ab51046d18b5396e70880cd8d4bb5cb19338
Ethan Egerton <egertonethan@gmail.com>
2021-10-14 13:27:19 +0100

    l

---
+++ EJS/Chapter-1-and-2/chapter-1.js
@@ -0,0 +1,37 @@
+// typeof
+console.log(typeof 4.5)
+console.log(typeof "x")
+
+console.log(- (10 - 2))
+
+// bool
+console.log(3 > 2)
+console.log(2 > 3)
+console.log("Aardvark" < "Zoroaster")
+console.log("Itchy" != "Scratchy")
+console.log("Apple" == "Orange")
+console.log(NaN == NaN)
+console.log(null == undefined)
+console.log(null == 0)
+
+// and
+console.log(true && false)
+console.log(false && true)
+
+// or
+console.log(false || true)
+console.log(true || false)
+
+// ternary
+console.log(true ? 1 : 2)
+console.log(false ? 1 : 2)
+
+// casting, type conversion
+console.log(8 * null)
+console.log("5" - 1)
+console.log("5" + 1)
+console.log("five" * 2)
+console.log(false == 0)
+
+console.log(null || "user")
+console.log("Agnes" || "user")
