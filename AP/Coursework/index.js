42d7449861b1c94c3efefa5d1a4fd4e943850405
Ethan Egerton <egertonethan@gmail.com>
2022-01-04 12:28:04 +0000

    lol

--- AP/Coursework/index.js
+++ AP/Coursework/index.js
@@ -2,10 +2,13 @@
 let wordsObj = {};
 let topic = ""
 let wordArray = []
-let splitWords = []
-const canvas = document.getElementById('canvas');
+let splitWord = []
+const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 let playerScore = 0
+let uniqueLetters = 0
+let correctLetters = 0
+let wordLength = 0
 
 // Data fetching
 fetch("./words.json")
@@ -19,34 +22,34 @@ fetch("./words.json")
 // Display Toggles
 function startToggle() {
     clearWordDisplay()
-    document.getElementById('topic-display').classList.add('display-none');
-    document.getElementById('btn-start').classList.add('display-none');
-    document.getElementById('topics').classList.add('display-none');
-    document.getElementById('letter-display').classList.remove('display-none');
-    document.getElementById('btn-end').classList.remove('display-none');
-    document.getElementById('keyboard').classList.remove('display-none')
+    document.querySelector('#topic-display').classList.add('display-none');
+    document.querySelector('#btn-start').classList.add('display-none');
+    document.querySelector('#topics').classList.add('display-none');
+    document.querySelector('#letter-display').classList.remove('display-none');
+    document.querySelector('#btn-end').classList.remove('display-none');
+    document.querySelector('#keyboard').classList.remove('display-none')
 }
 
 function endToggle() {
-    document.getElementById('topic-display').classList.remove('display-none');
-    document.getElementById('btn-start').classList.remove('display-none');
-    document.getElementById('topics').classList.remove('display-none');
-    document.getElementById('letter-display').classList.add('display-none');
-    document.getElementById('btn-end').classList.add('display-none');
-    document.getElementById('keyboard').classList.add('display-none')
+    document.querySelector('#topic-display').classList.remove('display-none');
+    document.querySelector('#btn-start').classList.remove('display-none');
+    document.querySelector('#topics').classList.remove('display-none');
+    document.querySelector('#letter-display').classList.add('display-none');
+    document.querySelector('#btn-end').classList.add('display-none');
+    document.querySelector('#keyboard').classList.add('display-none')
 }
 
 // Word Display 
 function clearWordDisplay() {
-    const element = document.getElementById("letter-display")
+    const element = document.querySelector("#letter-display")
     while (element.firstChild) {
     element.removeChild(element.firstChild);
     }
 }
 
 function removeKeyColours() {
-    const reds = document.getElementsByClassName('btn-red');
-    const greens = document.getElementsByClassName('btn-green');
+    const reds = document.querySelectorAll('.btn-red');
+    const greens = document.querySelectorAll('.btn-green');
     Array.from(reds).forEach(element => {
         element.classList.remove('btn-red');});
     Array.from(greens).forEach(element => {
@@ -56,7 +59,7 @@ function removeKeyColours() {
 // Word Selecting
 function selectTopic(event) {
     topic = event.currentTarget.id;
-    document.getElementById("topic-selected").textContent = topic;
+    document.querySelector("#topic-selected").textContent = topic;
     wordArray = wordsObj[topic]
 }
 
@@ -73,27 +76,32 @@ function randomWordGen() {
 // Letter Validation
 
 function checkLetters(event) {
+    // Cycles through every letter in array splitWord to check if element
+    // matches the letter.
     const letter = event.currentTarget.textContent;
-    if (splitWords.includes(letter)) {
-        event.currentTarget.classList.add('btn-green');
-        let letters = document.getElementsByClassName(letter)
-        Array.from(letters).forEach(element => {
-        element.textContent = letter;});
-    } else {
-        console.log("not it")
+    let foundLetter = false;
+    for (let i = 0; i < wordLength; i++) {
+        if (letter = splitWord[i]) {
+            correctLetters += 1
+            event.currentTarget.classList.add('btn-green');
+            foundLetter = true;
+        }
+    }
+    if (foundLetter == false) {
         event.currentTarget.classList.add('btn-red');
         event.currentTarget.removeEventListener('click', checkLetters);
         playerScore += 1;
     }
     canvasBuild(playerScore);
+    if (correctLetters === uniqueLetters) {GameWon()};
 }
 
 // Start / End
 function keyboardEventReset() {
-    let keyboardButtons = document.getElementsByClassName('btn-keyboard')
+    const keyboardButtons = document.querySelectorAll('.btn-keyboard')
     Array.from(keyboardButtons).forEach(element => {
     element.addEventListener('click', checkLetters);
-});   
+    });   
 }
 
 function startGame() {
@@ -103,24 +111,35 @@ function startGame() {
     const word = randomWordGen();
     playerScore = 0;
     canvasBuild();
-    splitWords = word.split("");
-    for (let i = 0; i < splitWords.length; i++) {
-        const letter = splitWords[i];
+
+    // Set letters in word display
+    splitWord = word.split("");
+    for (let i = 0; i < splitWord.length; i++) {
+
         let eleLetter = document.createElement("SPAN");
         eleLetter.textContent = String.fromCharCode(160);
         eleLetter.className = "letter";
-        eleLetter.classList.add(letter)
-        document.getElementById('letter-display').append(eleLetter);
+        eleLetter.classList.add(i)
+        document.querySelector('#letter-display').append(eleLetter);
         }
+
+    // Count unique letters
+    // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+    uniqueLetters = splitWord.filter((v, i, splitWord) => splitWord.indexOf(v) === i).length;
+    wordLength = splitWord.length;
 }
 
-function endGame() {
+function GameWon() {
+    endToggle();
+    removeKeyColours();
+}
+
+function GameLost() {
     endToggle();
     removeKeyColours();
 }
 
 function canvasBuild(score) {
-    console.log("func ran")
     switch(score){
         case 1:
             // Base
@@ -183,12 +202,12 @@ function canvasBuild(score) {
             ctx.moveTo(155,203);
             ctx.lineTo(136,243);
             ctx.stroke();
+            GameLost();
             break;
     }
-    console.log("func ends")
 }
 
-document.getElementById('btn-start').addEventListener('click', startGame)
-document.getElementById('btn-end').addEventListener('click', endGame)
-document.getElementById('food').addEventListener('click', selectTopic);
-document.getElementById('music').addEventListener('click', selectTopic);
+document.querySelector('#btn-start').addEventListener('click', startGame)
+document.querySelector('#btn-end').addEventListener('click', GameLost)
+document.querySelector('#food').addEventListener('click', selectTopic);
+document.querySelector('#music').addEventListener('click', selectTopic);
